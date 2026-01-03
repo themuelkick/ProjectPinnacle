318cfd3023c3810bc6946e47978b80e0dfaee1a5
themuelkick <mmueller4@rogers.com>
2026-01-03 12:19:16 -0600

    Delete drill_tags.py

--- drill_tags.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Table, Column, ForeignKey
-from app.db import Base
-
-drill_tags = Table(
-    "drill_tags",
-    Base.metadata,
-    Column("drill_id", ForeignKey("drills.id"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
-)
